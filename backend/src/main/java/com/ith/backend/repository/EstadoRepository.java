59d00412c7155e1331faea316c21decd4b9ebcb7
magalhaesmth <matheusurbano2002@hotmail.com>
2023-05-04 18:02:13 -0300

    Iniciando cruds

---
+++ backend/src/main/java/com/ith/backend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.ith.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ith.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository <Estado, Long>{
+    
+}
